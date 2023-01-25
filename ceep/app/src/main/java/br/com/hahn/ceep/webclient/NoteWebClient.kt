d9b63163773cafa7e27f7d1f38e0d7b982e9f434
andrehahn05 <andrehahn05@gmail.com>
2023-01-24 22:29:22 -0400

    props notesService

--- ceep/app/src/main/java/br/com/hahn/ceep/webclient/NoteWebClient.kt
+++ ceep/app/src/main/java/br/com/hahn/ceep/webclient/NoteWebClient.kt
@@ -3,13 +3,15 @@ package br.com.hahn.ceep.webclient
 import android.util.Log
 import br.com.hahn.ceep.model.Note
 import br.com.hahn.ceep.webclient.model.NoteResponse
+import br.com.hahn.ceep.webclient.services.NoteService
 
 private const val TAG = "NoteWebClient"
+private  val noteService: NoteService = RetrofitInitialize().service
 
 class NoteWebClient {
     suspend fun findAll() : List<Note>? {
         return try {
-            val noteResponse = RetrofitInitialize().service.findAll()
+            val noteResponse = noteService.findAll()
             return noteResponse.map(NoteResponse::note)
         }
         catch (e : Exception) {
